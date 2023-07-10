6cae8373d6ee8c51fa7acd4cd945b195b1099e00
HARSH JOLAPARA <harshj6680@gmail.com>
2023-07-10 16:26:24 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,6 +10,7 @@ import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  //hidden
 };
 
 // Initialize Firebase
